d342efe595e096e0c948a1856b8db248f47b7662
Sara Gowen <gowens@parliament.uk>
2024-03-22 09:48:51 +0000

    Add special values controller

---
+++ UserSecrets/Controllers/SpecialValueController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserSecrets.Controllers
+{    
+    [Route("[controller]")]
+    [ApiController]
+    public class SpecialValueController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SpecialValueController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _configuration["SpecialValue"]?? "No config value found";
+        }
+    }
+}
